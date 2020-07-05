075f70453e5a5cb08a6be5047b1137744b43acd2
preocts <preocts@gmail.com>
2020-07-05 16:34:31 -0400

    v1.0

--- README.md
+++ README.md
@@ -6,4 +6,15 @@ Why?
 # ╚═══════════════*.·: ·.✧    ✦    ✧.·: ·.*═══════════════╝
 ```
 
+```html
+<!--╔══════════════════*.·: ·. .·: ·.*══════════════════╗-->
+<!--       In computer programming, a comment is a       -->
+<!--  programmer-readable explanation or annotation in   -->
+<!--   the source code of a computer program. They are   -->
+<!--  added with the purpose of making the source code   -->
+<!-- easier for humans to understand, and are generally  -->
+<!--       ignored by compilers and interpreters.        -->
+<!--╚══════════════════*.·: ·. .·: ·.*══════════════════╝-->
+```
+
 More work planned here....
--- js/base.js
+++ js/base.js
@@ -18,36 +18,94 @@ function quickOut() {
 };
 
 function simpleComment() {
-    let commentBody = document.getElementById("tbCommentBody").value;
-    if (!(commentBody.length)) {
+    if (!(document.getElementById("tbCommentBody").value.length)) {
         document.getElementById("eggOutput").style.display = "none";
         return;
     };
-
+    let commentFinal = ""
     let maxWidth = document.getElementById("tbMaxWidth").value;
     let prefix = document.getElementById("tbCommentOpen").value;
     let postfix = document.getElementById("tbCommentClose").value;
-
     let commentSpace = maxWidth - (prefix.length + postfix.length);
+    console.log(maxWidth, commentSpace);
+
     if (commentSpace <= 0) {
         console.log("Not enough space ya fool of a Took!");
         return;
     };
 
-    let topBorder = prefix + _createBorder("╔", "═", "╗", "*.·: ·.✧    ✦    ✧.·: ·.*", commentSpace); + postfix;
-    let bottomBorder = prefix + _createBorder("╚", "═", "╝", "*.·: ·.✧    ✦    ✧.·: ·.*", commentSpace); + postfix;
+    // Split comment into lines that fit within the max width
+    let commentLines = buildLines(document.getElementById("tbCommentBody").value, commentSpace);
     
-    commentBody = prefix + " ".repeat((commentSpace - commentBody.length) / 2) + commentBody + postfix;
-    commentBody = topBorder + "\n" + commentBody + "\n" + bottomBorder;
-
+    // Add Top
+    commentFinal += `${prefix}${createBorder("top", commentSpace)}${postfix}\n`;
+    // Add Comment Lines
+    for (let idx = 0; idx < commentLines.length; idx++) {
+        commentFinal += `${prefix}${bufferCenter(commentLines[idx], commentSpace)}${postfix}\n`;
+    }
+    // Add Bottom
+    commentFinal += `${prefix}${createBorder("bottom", commentSpace)}${postfix}`;
+    
+    // Displays debug bar
+    // let debugBar = "-".repeat(maxWidth);
+    // commentFinal = `${debugBar}\n${commentFinal}\n${debugBar}`
+    
     document.getElementById("eggOutput").style.display = "block";
-    document.getElementById("eggOutput").textContent = commentBody;
+    document.getElementById("eggOutput").textContent = commentFinal;
 
 };
 
-function _createBorder(_left, _bar, _right, _center, _size) {
+function createBorder(_type, _size) {
+    let topLeft =     "╔";
+    let topRight =    "╗";
+    let bottomLeft =  "╚";
+    let bottomRight = "╝";
+    let borderPiece = "═";
+    let centerPiece = "*.·: ·. .·: ·.*";
+    let _left, _right = "";
+
+    if (_type.toLowerCase() == "top") {
+        _left = topLeft;
+        _right = topRight;
+    };
+    if (_type.toLowerCase() == "bottom") {
+        _left = bottomLeft;
+        _right = bottomRight;
+    };
+    
     // How wide does each bar need to be?
-    barWidth = (_size - (_left.length + _right.length + _center.length)) / 2;
-    let return_value = _left + _bar.repeat(barWidth) + _center + _bar.repeat(barWidth) + _right;
-    return return_value;
+    barWidth = (_size - (_left.length + _right.length + centerPiece.length)) / 2;
+
+    return `${_left}${borderPiece.repeat(barWidth)}${centerPiece}${borderPiece.repeat(barWidth)}${_right}`;
+};
+
+function bufferCenter(commentText, commentSpace) {
+    // Centers our text using spaces. Extra space is added to the end
+    if (commentText.length > commentSpace){
+        return commentText;
+    };
+    // Leading spaces
+    return_text = " ".repeat((commentSpace - commentText.length) / 2);
+    // Add commentText
+    return_text += commentText;
+    // Trailing spaces so that return_text.length == commentSpace
+    return_text += " ".repeat(commentSpace - (return_text.length));
+    return return_text;
+};
+
+function buildLines(commentBody, maxWidth) {
+    let words = commentBody.split(' ');
+    let line = "";
+    let outLines = [];
+    for (let idx = 0; idx < words.length; idx++) {
+        if ((line + " " + words[idx] + " ").length > maxWidth) {
+            outLines.push(line + " ");
+            line = "";
+        };
+        line += " " + words[idx];
+    };
+    if (line.length) {
+        outLines.push(line + " ");
+    };
+    return outLines;
 };
